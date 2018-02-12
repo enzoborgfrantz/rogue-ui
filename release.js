f5067236a03c4b209783c4b6ce8398b31de9358b
enzoborgfrantz <enzo.frantz@goeuro.com>
2018-02-13 00:14:58 +0100

    release

--- release.js
+++ release.js
@@ -4,11 +4,14 @@ const { rename } = require('fs');
 const release = () => {
   execSync('yarn run styleguide:build');
   execSync('yarn run build');
-  rename('./styleguide', 'docs/styleguide', err => {
+  rename('./styleguide', './docs/styleguide', err => {
     if (err) {
       console.error(error);
     } else {
       console.log('completed');
+      execSync('git add .');
+      execSync('git commit -m "release"');
+      execSync('git push');
     }
   });
 };
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,4 +1,6 @@
-export const gray = '#2A363B';
-export const red = '#CF4647';
+// export const gray = '#2A363B';
+// export const red = '#CF4647';
+export const gray = '#212125';
+export const red = '#17B978';
 export const yellow = '#F5D061';
 export const white = '#F8F6F6';
